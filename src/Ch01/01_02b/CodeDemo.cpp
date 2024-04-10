322c68c9e779f2e9f5cfc8013aca3ed89390eb32
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-10 18:21:08 +0000

    Completed Chapter1-Video2

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
-// Learning C++ 
+// Learning C++
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Eduardo Corpeño
+
+#include <iostream>
+
+// Greets the user and returns 0 when done.
+int main() {
+  std::cout << "Hi There!" << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
+}
