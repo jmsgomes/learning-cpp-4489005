97e755c524dc3fae5ea83cbc56def657f638c86a
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 23:41:11 +0000

    Complete Exercise 04_02

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -1,24 +1,40 @@
-// Learning C++ 
+// Learning C++
 // Exercise 04_02
-// Switch Statements, by Eduardo Corpeño 
+// Switch Statements, by Eduardo Corpeño
 
 #include <iostream>
 
-int main(){
-    float operand_1, operand_2, result;
-    char operation;
+int main() {
+  float operand_1, operand_2, result;
+  char operation;
 
-    std::cout << "Enter operand 1: " << std::flush;
-    std::cin >> operand_1;
-    std::cout << "Enter operand 2: " << std::flush;
-    std::cin >> operand_2;
-    std::cout << "Choose operation [ + - * / ]: " << std::flush;
-    std::cin >> operation;
+  std::cout << "Enter operand 1: " << std::flush;
+  std::cin >> operand_1;
+  std::cout << "Enter operand 2: " << std::flush;
+  std::cin >> operand_2;
+  std::cout << "Choose operation [ + - * / ]: " << std::flush;
+  std::cin >> operation;
 
-    // switch goes here
+  switch (operation) {
+    case '+':
+      result = operand_1 + operand_2;
+      break;
+    case '-':
+      result = operand_1 - operand_2;
+      break;
+    case '*':
+      result = operand_1 * operand_2;
+      break;
+    case '/':
+      result = operand_1 / operand_2;
+      break;
+    default:
+      std::cout << "Invalid operation." << std::endl;
+      return 1;
+  }
 
-    std::cout << "The result is " << result << std::endl;
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+  std::cout << "The result is " << result << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
