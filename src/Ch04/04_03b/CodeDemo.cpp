f08bdc48c32198eed462b35d0d20aec5071ad833
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 23:48:40 +0000

    Complete Exercise 04_03

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -1,15 +1,27 @@
-// Learning C++ 
+// Learning C++
 // Exercise 04_03
-// While Loops, by Eduardo Corpeño 
+// While Loops, by Eduardo Corpeño
 
 #include <iostream>
 #include <vector>
 
-int main(){
-    std::vector<int> numbers = {12, 25, 31, 47, 58};
-    
-    std::vector<int>::iterator ptr = numbers.begin();
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+int main() {
+  std::vector<int> numbers = {12, 25, 31, 47, 58};
+
+  std::vector<int>::iterator ptr = numbers.begin();
+  while (ptr != numbers.end()) {
+    std::cout << *ptr << " ";
+    ptr = next(ptr, 1);
+  }
+  std::cout << std::endl;
+
+  int i = 0;
+  do {
+    std::cout << numbers[i] << " ";
+    i++;
+  } while (i < numbers.size());
+  std::cout << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
