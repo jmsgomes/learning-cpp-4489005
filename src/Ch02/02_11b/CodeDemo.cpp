16195c432fe5ca5dea8067f1a298561b57ce5601
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 05:40:17 +0000

    Complete Chapter2-Video11

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -1,12 +1,26 @@
-// Learning C++ 
+// Learning C++
 // Exercise 02_11
-// Type Casting Examples, by Eduardo Corpeño 
+// Type Casting Examples, by Eduardo Corpeño
 
-#include <iostream>
 #include <cstdint>
+#include <iostream>
 
-int main(){
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+int main() {
+  int fahrenheit = 100;
+  int celsius;
+
+  celsius = (fahrenheit - 32) * (float)5 / 9.0;
+  std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+  std::cout << "Celsius: " << celsius << std::endl;
+  std::cout << fahrenheit << " degrees Fahrenheit is " << celsius
+            << " degrees Celsius." << std::endl;
+
+  double weight = 10.99;
+  std::cout << "Float:         : " << weight << std::endl;
+  std::cout << "Integer part:  : " << (int)weight << std::endl;
+  std::cout << "Fractional part: " << (int)((weight - (int)weight) * 10000)
+            << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
