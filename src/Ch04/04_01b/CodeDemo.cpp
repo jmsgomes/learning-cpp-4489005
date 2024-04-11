d101d711695af2e5d11537f8a3b9abe80654c80e
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 23:34:42 +0000

    Complete Exercise 04_01

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -1,11 +1,35 @@
-// Learning C++ 
+// Learning C++
 // Exercise 04_01
-// If Statements, by Eduardo Corpeño 
+// If Statements, by Eduardo Corpeño
 
 #include <iostream>
 
-int main(){
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+int main() {
+  int a = 1023;
+  bool flag = false;
+  char lttr = 'd';
+  if (a > 1000) {
+    std::cout << "Warning: a is over 1000" << std::endl;
+  }
+
+  if ((a % 2) == 1) {
+    std::cout << "a is odd" << std::endl;
+  } else {
+    std::cout << "a is even" << std::endl;
+  }
+
+  std::cout << "The letter " << lttr << " is ";
+  if (lttr != 'a' && lttr != 'e' && lttr != 'i' && lttr != 'o' && lttr != 'u' &&
+      lttr != 'A' && lttr != 'E' && lttr != 'I' && lttr != 'O' && lttr != 'U') {
+    std::cout << "not";
+  }
+  std::cout << " a vowel" << std::endl;
+
+  if (flag) {
+    std::cout << "Thr flag is true!" << std::endl;
+  } else {
+    std::cout << "The flag is false!" << std::endl;
+  }
+  std::cout << std::endl << std::endl;
+  return 0;
 }
