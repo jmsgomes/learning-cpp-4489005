e7531ac5eedc19e8463f6981d23ed8b1607e083f
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 05:57:34 +0000

    Complete Chapter2-Video13

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -1,17 +1,19 @@
-// Learning C++ 
+// Learning C++
 // Challenge 02_13
-// Calculate an Average, by Eduardo Corpeño 
+// Calculate an Average, by Eduardo Corpeño
 
 #include <iostream>
 
-int main(){
-    int nums[5] = {1,23,32,24,337};
-    float result; 
+int main() {
+  int nums[5] = {1, 23, 32, 24, 337};
+  float result;
 
-    // Write your code here
-    
-    std::cout << "The average is " << result << std::endl;
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+  // Write your code here
+  result = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+  result /= 5;
+
+  std::cout << "The average is " << result << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
