4aa868150a9bcfddb9797e7bf8bda6067d8e9b3d
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 18:10:48 +0000

    Complete Exercise 03_05

---
+++ src/Ch03/03_05b/cow.cpp
@@ -0,0 +1,15 @@
+#include "cow.h"
+
+cow::cow(std::string name_i, int age_i, cow_purpose purpose_i) {
+  name = name_i;
+  age = age_i;
+  purpose = purpose_i;
+}
+
+std::string cow::get_name() const { return name; }
+
+int cow::get_age() const { return age; }
+
+void cow::set_age(int new_age) { age = new_age; }
+
+cow_purpose cow::get_purpose() const { return purpose; }
---
+++ src/Ch03/03_05b/cow.h
@@ -0,0 +1,26 @@
+#ifndef COW_H
+#define COW_H
+#endif  // COW_H
+
+#include <string>
+
+enum class cow_purpose {
+  dairy,
+  meat,
+  hide,
+  pet,
+};
+
+class cow {
+ public:
+  cow(std::string name_i, int age_i, cow_purpose purpose_i);
+  std::string get_name() const;
+  int get_age() const;
+  cow_purpose get_purpose() const;
+  void set_age(int new_age);
+
+ private:
+  std::string name;
+  int age;
+  cow_purpose purpose;
+};
