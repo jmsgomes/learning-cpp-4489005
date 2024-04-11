40348280d4c8d9163dc396be70d294e9b0b08900
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 16:06:13 +0000

    Complete Exercise 03_02

--- src/Ch03/03_02b/CodeDemo.cpp
+++ src/Ch03/03_02b/CodeDemo.cpp
@@ -1,26 +1,47 @@
-// Learning C++ 
+// Learning C++
 // Exercise 03_02
-// Classes, by Eduardo Corpeño 
+// Classes, by Eduardo Corpeño
 
 #include <iostream>
 #include <string>
 
-enum class cow_purpose {dairy, meat, hide, pet};
-
-struct cow{
-    std::string name;
-    int age;
-    cow_purpose purpose;
+enum class cow_purpose {
+  dairy,
+  meat,
+  hide,
+  pet,
 };
 
-int main(){
-    cow my_cow;
-    my_cow.age = 5;
-    my_cow.name = "Betsy";
-    my_cow.purpose = cow_purpose::dairy;
-    std::cout << my_cow.name << " is a type-" << (int)my_cow.purpose << " cow." << std::endl;
-    std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+class cow {
+ public:
+  cow(std::string name_i, int age_i, cow_purpose purpose_i) {
+    name = name_i;
+    age = age_i;
+    purpose = purpose_i;
+  }
+  std::string get_name() const { return name; }
+  int get_age() const { return age; }
+  cow_purpose get_purpose() const { return purpose; }
+  void set_age(int value) { age = value; }
+  void set_name(std::string value) { name = value; }
+  void set_purpose(cow_purpose value) { purpose = value; }
+
+ private:
+  std::string name;
+  int age;
+  cow_purpose purpose;
+};
+
+int main() {
+  cow my_cow = cow("Daisy", 5, cow_purpose::dairy);
+  // my_cow.set_age(5);
+  // my_cow.set_name("Betsy");
+  // my_cow.set_purpose(cow_purpose::dairy);
+  std::cout << my_cow.get_name() << " is a type-" << (int)my_cow.get_purpose()
+            << " cow." << std::endl;
+  std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old."
+            << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
