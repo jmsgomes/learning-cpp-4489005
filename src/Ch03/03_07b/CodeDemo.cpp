4a7c4383eee59d69d51c004143966615aa16c26a
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 19:02:40 +0000

    Complete Exercise 03_07

--- src/Ch03/03_07b/CodeDemo.cpp
+++ src/Ch03/03_07b/CodeDemo.cpp
@@ -1,16 +1,20 @@
-// Learning C++ 
+// Learning C++
 // Exercise 03_07
-// Using Objects with Pointers, by Eduardo Corpeño 
+// Using Objects with Pointers, by Eduardo Corpeño
 
 #include <iostream>
 #include <string>
 #include "cow.h"
 
 int main(){
-    cow my_cow("Hildy", 7, cow_purpose::pet);
-    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
-    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+    cow *my_cow;
+    my_cow = new cow("Gertie", 3, cow_purpose::hide);
+    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
+    std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
+
+    delete my_cow;
+    my_cow = nullptr;
     
     std::cout << std::endl << std::endl;
-    return (0);
+    return 0;
 }
