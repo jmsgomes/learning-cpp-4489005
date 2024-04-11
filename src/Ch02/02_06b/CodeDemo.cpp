ded730a58693703cde35eb76d9a538a0530a14f3
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-11 01:07:20 +0000

    Complete Chapter2-Video6

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -1,11 +1,25 @@
-// Learning C++ 
+// Learning C++
 // Exercise 02_06
-// Preprocessor directives, by Eduardo Corpeño 
+// Preprocessor directives, by Eduardo Corpeño
 
+#include <cstdint>
 #include <iostream>
+#include <string>
 
-int main(){
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+#define CAPACITY 5000
+#define DEBUG true
+
+int main() {
+  int32_t large = CAPACITY;
+  uint8_t small = 37;
+
+#ifdef DEBUG
+  std::cout << "[About to perform the addition]" << std::endl;
+#endif
+
+  large += small;  // Shorthand for "large = large + small;"
+  std::cout << "The large integer is: " << large << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
