227947248568d37a661504d9e8a12399268b9e59
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-12 20:25:20 +0000

    Complete Exercise 06_02

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -1,27 +1,27 @@
-// Learning C++ 
+// Learning C++
 // Exercise 06_02
-// Opening a text file for writing, by Eduardo Corpeño 
+// Opening a text file for writing, by Eduardo Corpeño
 
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
-int main(){
-    std::ofstream outFile;
-    float a = 4.333f, b = 5.302f;
+int main() {
+  std::ofstream outFile;
+  float a = 4.333f, b = 5.302f;
 
-    outFile.open("calculations.txt");
-    if (outFile.fail())
-        std::cout << std::endl << "Couldn't open the file!" << std::endl;
-    else{
-        outFile << "a = " << a << std::endl;
-        outFile << "b = " << b << std::endl;
-        outFile << "a + b = " << a + b << std::endl;
-        outFile << "a * b = " << a * b << std::endl;
-        outFile.close();
-        std::cout << "File written successfully!" << std::endl;
-    }
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+  outFile.open("calculations.txt");
+  if (outFile.fail())
+    std::cout << std::endl << "Couldn't open the file!" << std::endl;
+  else {
+    outFile << "a = " << a << std::endl;
+    outFile << "b = " << b << std::endl;
+    outFile << "a + b = " << a + b << std::endl;
+    outFile << "a * b = " << a * b << std::endl;
+    outFile.close();
+    std::cout << "File written successfully!" << std::endl;
+  }
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
