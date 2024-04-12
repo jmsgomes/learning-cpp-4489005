0fd600449c21a4f0e8049a7a2ea1f6ef3c63d9b6
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-12 01:07:12 +0000

    Complete Exercise 05_02

--- src/Ch05/05_02b/CodeDemo.cpp
+++ src/Ch05/05_02b/CodeDemo.cpp
@@ -1,18 +1,41 @@
-// Learning C++ 
+// Learning C++
 // Exercise 05_02
-// Function Parameters, by Eduardo Corpeño 
+// Function Parameters, by Eduardo Corpeño
 
 #include <iostream>
 
-int main(){
-    int a = 9, b;
-    // TODO: square
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    
-    std::cout << std::endl << std::endl;
-    return (0);
+// Take arguments by value
+int square(int x) {
+  x = x * x;
+  return x;
+}
+
+// Takes arguments by address
+void swap(int* x, int* y) {
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+// Takes arguments by reference
+void swap(int& x, int& y) {
+  int temp = x;
+  x = y;
+  y = temp;
+}
+
+int main() {
+  int a = 9, b;
+  // TODO: square
+  b = square(a);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  // TODO: swap
+  swap(&a, &b);
+   std::cout << "a = " << a << ", b = " << b << std::endl;
+  // TODO: swap
+  swap(&a, &b);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+
+  std::cout << std::endl << std::endl;
+  return 0;
 }
